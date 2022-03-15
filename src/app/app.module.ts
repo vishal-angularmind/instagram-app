4972db638a600fc70ccb7e166ca557b1f8cf2933
vishal-angularminds <vishal@angularminds.in>
2022-03-15 14:47:22 +0530

    files updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './layouts/user/user.component';
-import { RegisterComponent } from './user/register/register.component';
-import { LoginComponent } from './user/login/login.component'
+
 import { HttpService } from './services/http.service';
-import { OtpComponent } from './user/otp/otp.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SetPasswordComponent } from './user/set-password/set-password.component';
-import { ProfileComponent } from './user/profile/profile.component';
+
 import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    RegisterComponent,
-    LoginComponent,
-    OtpComponent,
-    SetPasswordComponent,
-    ProfileComponent
+
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/user/user.module.ts
+++ src/app/layouts/user/user.module.ts
@@ -4,17 +4,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { UserRoutes } from './user-routing.module';
 import { RouterModule } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
+import { UserComponent } from './user.component';
+import { RegisterComponent } from 'src/app/user/register/register.component';
+import { LoginComponent } from 'src/app/user/login/login.component';
+import { OtpComponent } from 'src/app/user/otp/otp.component';
+import { SetPasswordComponent } from 'src/app/user/set-password/set-password.component';
+import { ProfileComponent } from 'src/app/user/profile/profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-  
+    UserComponent,
+    RegisterComponent,
+    LoginComponent,
+    OtpComponent,
+    SetPasswordComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(UserRoutes),
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [HttpService],
 })
